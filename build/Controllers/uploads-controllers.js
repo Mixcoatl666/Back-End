1d8d7607498f41ef56c620f3c1b210c9409e1de0
Mixcoatl666 <116208536+Mixcoatl666@users.noreply.github.com>
2023-08-10 20:45:43 -0600

    Uploads Beta Final v0.0.9

--- build/Controllers/uploads-controllers.js
+++ build/Controllers/uploads-controllers.js
@@ -10,19 +10,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.uploadsController = void 0;
+const database_1 = require("../database");
 class UploadsController {
     createPhoto(req, res) {
+        var _a;
         return __awaiter(this, void 0, void 0, function* () {
-            console.log('Guardando Foto');
-            console.log(req.body);
-            return res.json({
-                message: 'Imagen Subida Correctamente'
-            });
+            const { title, description } = req.body;
+            const newPhoto = {
+                title: title,
+                description: description,
+                filePath: (_a = req.file) === null || _a === void 0 ? void 0 : _a.path
+            };
+            try {
+                const connection = yield database_1.pool.getConnection(); // Obtener una conexión del pool
+                // Realizar la inserción en la base de datos
+                yield connection.query('INSERT INTO tb_galeria (titulo, descripcion, imagenRuta) VALUES (?, ?, ?)', [newPhoto.title, newPhoto.description, newPhoto.filePath]);
+                connection.release(); // Liberar la conexión
+                return res.json({
+                    message: 'Imagen Subida Correctamente'
+                });
+            }
+            catch (error) {
+                console.error('Error al insertar en la base de datos:', error);
+                return res.status(500).json({
+                    message: 'Error al subir la imagen'
+                });
+            }
         });
     }
     getPhoto(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            ``;
+            try {
+                const result = yield database_1.pool.query('SELECT * FROM tb_galeria');
+                res.json(result[0]);
+            }
+            catch (error) {
+                if (error instanceof Error) {
+                    console.error('Error al obtener tipos:', error);
+                    res.status(500).json({ error: 'Error al obtener tipos', details: error.message });
+                }
+                else {
+                    console.error('Error al obtener tipos:', error);
+                    res.status(500).json({ error: 'Error al obtener tipos' });
+                }
+            }
         });
     }
 }
--- src/Controllers/uploads-controllers.ts
+++ src/Controllers/uploads-controllers.ts
@@ -3,19 +3,49 @@ import { pool } from "../database";
 
 class UploadsController{
 
-  async createPhoto(req: Request, res: Response){
-
-    console.log('Guardando Foto')
-    console.log(req.body)
-    
-    return res.json({
-      message: 'Imagen Subida Correctamente'
-    })
+  async createPhoto(req: Request, res: Response) {
+    const { title, description } = req.body;
+  
+    const newPhoto = {
+      title: title,
+      description: description,
+      filePath: req.file?.path
+    }
+  
+    try {
+      const connection = await pool.getConnection(); // Obtener una conexión del pool
+  
+      // Realizar la inserción en la base de datos
+      await connection.query('INSERT INTO tb_galeria (titulo, descripcion, imagenRuta) VALUES (?, ?, ?)', [newPhoto.title, newPhoto.description, newPhoto.filePath]);
+  
+      connection.release(); // Liberar la conexión
+  
+      return res.json({
+        message: 'Imagen Subida Correctamente'
+      });
+    } catch (error) {
+      console.error('Error al insertar en la base de datos:', error);
+      return res.status(500).json({
+        message: 'Error al subir la imagen'
+      });
+    }
+  }
+  
+  async getPhoto(req:Request, res:Response) {
+    try {
+      const result = await pool.query('SELECT * FROM tb_galeria');
+      res.json(result[0]);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error al obtener tipos:', error);
+        res.status(500).json({ error: 'Error al obtener tipos', details: error.message });
+      } else {
+        console.error('Error al obtener tipos:', error);
+        res.status(500).json({ error: 'Error al obtener tipos' });
+      }
+    }
   }
 
-  async getPhoto(req: Request, res: Response){
-``
-  }
 }
 
 export const uploadsController = new UploadsController();
